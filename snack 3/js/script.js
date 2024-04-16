a35a49857c464048b8f29139383eddc707d308c4
OmarThierno <thiernomarndiaye76@gmail.com>
2024-04-16 12:12:39 +0200

    fix to fix

--- snack 3/js/script.js
+++ snack 3/js/script.js
@@ -1,3 +1,37 @@
-function newArray(array, val1, val1) {
-  
-}
+const automobili = [
+  { marca: "Toyota", modello: "Yaris", alimentazione: "ibrida" },
+  { marca: "Volkswagen", modello: "Golf", alimentazione: "benzina" },
+  { marca: "BMW", modello: "i3", alimentazione: "elettrico" },
+  { marca: "Fiat", modello: "Panda", alimentazione: "metano" },
+  { marca: "Renault", modello: "Clio", alimentazione: "diesel" },
+  { marca: "Tesla", modello: "Model S", alimentazione: "elettrico" },
+  { marca: "Ford", modello: "Focus", alimentazione: "benzina" },
+  { marca: "Audi", modello: "A3", alimentazione: "diesel" },
+  { marca: "Mercedes-Benz", modello: "EQC", alimentazione: "elettrico" },
+  { marca: "Opel", modello: "Corsa", alimentazione: "gpl" }
+];
+
+
+const autoBen = [];
+const autoDie = [];
+const autoGpl = [];
+const autoEle = [];
+const autoMet = [];
+
+automobili.forEach((curAuto, index) => {
+  const {alimentazione, marca} = automobili;
+
+  if (curAuto.alimentazione === 'benzina') {
+    autoBen.push(curAuto)
+  } else if (curAuto.alimentazione === 'diesel') {
+    autoDie.push(curAuto)
+  } else if (curAuto.alimentazione === 'gpl') {
+    autoGpl.push(curAuto)
+  } else if (curAuto.alimentazione === 'elettrico') {
+    autoEle.push(curAuto)
+  } else if (curAuto.alimentazione === 'metano') {
+    autoMet.push(curAuto)
+  } 
+})
+
+console.log(autoBen);
